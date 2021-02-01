b4a04f2d757b65cbffeffc9be1ed4887b987ef77
Chris Schuster <schustercp@gmail.com>
2021-02-01 09:50:43 -0600

    Updated Aranz Layer to support this version of u-boot

--- board/aranz/silhouettestar2/silhouettestar2.c
+++ board/aranz/silhouettestar2/silhouettestar2.c
@@ -15,23 +15,28 @@
  * SPDX-License-Identifier:	GPL-2.0+
  */
 
-#include <common.h>
-#include <init.h>
-#include <asm/io.h>
 #include <asm/arch/clock.h>
 #include <asm/arch/imx-regs.h>
 #include <asm/arch/iomux.h>
 #include <asm/arch/mx6-pins.h>
-#include <env.h>
+#include <asm/arch/mxc_hdmi.h>
 #include <linux/errno.h>
 #include <asm/gpio.h>
 #include <asm/mach-imx/iomux-v3.h>
+#include <asm/mach-imx/sata.h>
+#include <asm/mach-imx/video.h>
+#include <environment.h>
 #include <mmc.h>
 #include <fsl_esdhc.h>
+#include <malloc.h>
 #include <miiphy.h>
-#include <asm/arch/sys_proto.h>
+#include <netdev.h>
 #include <asm/arch/crm_regs.h>
+#include <asm/io.h>
+#include <asm/arch/sys_proto.h>
 #include <spl.h>
+#include <usb.h>
+#include <usb/ehci-ci.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
